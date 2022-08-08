a1b2bf101327b9c89d694cd45f3c61ef5f99075d
Beatriz Malveiro <beatriz@interactivethings.com>
2022-08-09 00:05:17 +0200

    change dialog to separate page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,69 @@
-import * as React from "react";
+import React, { useState, useMemo } from "react";
+import { css } from "@emotion/react";
+import { motion } from "framer-motion";
+import { flag } from "country-emoji";
+import Link from "next/link";
 
-import AllCountries from "../components/allCountries";
+import Controls from "../components/Controls";
+import { Flag } from "../components/Flag";
+import { getData } from "../data";
+import { useFlagDimensions } from "../hooks/useFlagDimensions";
 
-function App() {
+function Home() {
+  const [year, setYear] = useState(2022);
+  const [orderAlphabetical, setOrderAlphabetical] = useState(true);
+  const [orderRanking, setOrderRanking] = useState(true);
+  const [sortDict, setSortDict] = useState({
+    type: "alphabetical",
+    ascending: true,
+  });
+  const [flagsContainer, flagDimensions] = useFlagDimensions();
+
+  const data = useMemo(
+    () => getData({ years: [year], sortingParams: sortDict }),
+    [year, sortDict]
+  );
+  const countries = data.map((d) => d.country);
+
   return (
-    <div className="w-3/4 mx-auto mt-8">
-      <header className="text-center">
-        <h1 className="text-3xl font-bold">🏳️‍🌈Flags of inequality🏳️‍🌈</h1>
-      </header>
-      <div>
-        <AllCountries />
+    <>
+      <Controls
+        year={year}
+        setYear={setYear}
+        orderRanking={orderRanking}
+        setOrderRanking={setOrderRanking}
+        orderAlphabetical={orderAlphabetical}
+        setOrderAlphabetical={setOrderAlphabetical}
+        sortDict={sortDict}
+        setSortDict={setSortDict}
+      />
+      <div
+        className="flex flex-row flex-wrap justify-center gap-4"
+        ref={flagsContainer}
+      >
+        {countries.map((country) => {
+          return (
+            <motion.div key={country} className="mt-4" layout>
+              <Link href={`/timeline/${encodeURIComponent(country)}`}>
+                <motion.div
+                  style={{
+                    ...flagDimensions,
+                    cursor: "pointer",
+                  }}
+                >
+                  <Flag country={country} year={year} />
+                </motion.div>
+              </Link>
+
+              <h3 className="font-medium text-slate-800">{`${
+                flag(country) || ""
+              } ${country}`}</h3>
+            </motion.div>
+          );
+        })}
       </div>
-    </div>
+    </>
   );
 }
 
-export default App;
+export default Home;
---
+++ pages/timeline/[country].js
@@ -0,0 +1,60 @@
+import { useRouter } from "next/router";
+import React, { useMemo, useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import { flag } from "country-emoji";
+
+import { getData } from "../../data";
+import { useFlagDimensions } from "../../hooks/useFlagDimensions";
+import { Flag } from "../../components/Flag";
+
+export default function Timeline() {
+  const [isVertical, setIsVertical] = useState(false);
+
+  useEffect(() => {
+    setIsVertical(window.innerWidth < 400);
+  }, []);
+
+  const router = useRouter();
+  const { country } = router.query;
+  const data = useMemo(() => getData({ countries: [country] }), [country]);
+  const years = data.map((d) => d.year).sort();
+  const [flagsContainer, flagDimensions] = useFlagDimensions(
+    isVertical ? 1 : years.length
+  );
+
+  console.log(isVertical);
+  return (
+    <div className="mt-4">
+      <h2 className="text-3xl font-medium text-slate-800 ">{`${
+        flag(country) || ""
+      } ${country}`}</h2>
+      <div
+        className={`flex ${
+          isVertical ? "flex-col" : "flex-row"
+        }  w-10/12 gap-4 mt-4`}
+        ref={flagsContainer}
+      >
+        {years.map((year) => (
+          <div
+            key={`${country}-${year}`}
+            className={`flex ${
+              isVertical ? "flex-row-reverse justify-end" : "flex-col"
+            } gap-4`}
+            style={isVertical ? { marginTop: "20%" } : {}}
+          >
+            <Box
+              /* This rotation doesn't work particularly well for the layout. @TODO: implement rotation prop on Flag. */
+              className={isVertical && "rotate-90"}
+              sx={{
+                ...flagDimensions,
+              }}
+            >
+              <Flag country={country} year={year} />
+            </Box>
+            <h3>{year}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
