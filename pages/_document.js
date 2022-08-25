2a51b498dab45d2fd44fc2b564b74c6dd43c5977
Beatriz Malveiro <beatriz@interactivethings.com>
2022-08-25 20:36:11 +0200

    add meta tags and preview image

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,39 @@ export default function Document() {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏳️‍🌈</text></svg>"
         />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta
+          name="twitter:image:alt"
+          content="Webpage showing a rainbow per country in a grid, some stripes of the flags are smaller or non-existent."
+        />
+
+        {/* Open Graph */}
+        <meta
+          property="og:url"
+          content={"www.flagsofinequality.com"}
+          key="ogurl"
+        />
+        <meta property="og:image" content={""} key="ogimage" />
+        <meta
+          property="og:site_name"
+          content={"Flags of Inequality"}
+          key="ogsitename"
+        />
+        <meta
+          property="og:title"
+          content={"Flags of Inequality"}
+          key="ogtitle"
+        />
+        <meta
+          property="og:description"
+          content={
+            "A visualization of the rainbow flag to portray to what extent different dimensions of queer life are disregarded by state regulations."
+          }
+          key="ogdesc"
+        />
+        <meta property="og:type" content="article" />
       </Head>
       <body className="mt-12 mb-20 bg-slate-800 text-slate-50">
         <Main />
