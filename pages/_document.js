3c18b48f2eb21bfb9d441a850e9dd086ffa17044
Beatriz Malveiro <beatriz@interactivethings.com>
2022-08-26 15:26:06 +0200

    retry twitter card

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
         />
 
         {/* Twitter */}
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:image"
           content="https://raw.githubusercontent.com/ritacosta0/flags-of-inequality/main/assets/preview.png"
@@ -27,7 +27,7 @@ export default function Document() {
         {/* Open Graph */}
         <meta
           property="og:url"
-          content={"https://www.flags-of-inequality.com"}
+          content="https://www.flags-of-inequality.com"
           key="ogurl"
         />
         <meta
