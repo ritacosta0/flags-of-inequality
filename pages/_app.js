0099e8c6a0e71d5026c7b077c63c5714d468f31b
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2023-05-12 08:45:06 +0100

    feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="w-3/4 mx-auto mt-8">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div className="w-3/4 mx-auto mt-8">
+        <Component {...pageProps} />
+      </div>
+      <Analytics />
+    </>
   );
 }
 
