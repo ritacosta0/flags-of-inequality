35c376201742184bba5e566cb42197f26af17284
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2024-05-19 16:37:20 +0100

    fix: update intro flag years to 2024

--- components/Intro/Intro.js
+++ components/Intro/Intro.js
@@ -77,7 +77,7 @@ function Intro() {
                   exit={{ opacity: 0 }}
                   transition={{ ease: "easeOut" }}
                 >
-                  <Flag country={"Malta"} year={2023} isInteractive={false} />
+                  <Flag country={"Malta"} year={2024} isInteractive={false} />
                 </motion.div>
               )}
               {currentStepIndex === 9 && (
@@ -90,7 +90,7 @@ function Intro() {
                   exit={{ opacity: 0 }}
                   transition={{ ease: "easeOut" }}
                 >
-                  <Flag country={"Russia"} year={2023} isInteractive={false} />
+                  <Flag country={"Russia"} year={2024} isInteractive={false} />
                 </motion.div>
               )}
             </AnimatePresence>
