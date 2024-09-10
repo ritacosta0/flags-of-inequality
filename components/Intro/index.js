696a27aeece1447ac630fca169a81fc8926efa8c
Beatriz Malveiro <mbeatrizmalveiro@gmail.com>
2024-09-10 14:06:13 +0100

    fix: intro year

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -74,7 +74,7 @@ function Intro() {
                 exit={{ opacity: 0 }}
                 transition={{ ease: "easeOut" }}
               >
-                <Flag country={"Malta"} year={2023} isInteractive={false} />
+                <Flag country={"Malta"} year={2024} isInteractive={false} />
               </motion.div>
             )}
             {currentStepIndex === 9 && (
@@ -87,7 +87,7 @@ function Intro() {
                 exit={{ opacity: 0 }}
                 transition={{ ease: "easeOut" }}
               >
-                <Flag country={"Russia"} year={2023} isInteractive={false} />
+                <Flag country={"Russia"} year={2024} isInteractive={false} />
               </motion.div>
             )}
           </AnimatePresence>
